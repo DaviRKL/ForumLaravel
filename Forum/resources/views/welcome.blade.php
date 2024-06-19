aa31b2f42eea99792cab214f689f4383cc6ed4aa
DaviRKL <davirkl07@gmail.com>
2024-06-19 10:10:08 -0300

    Retirei os arquivos relacionados ao react, ts e node

--- Forum/resources/views/welcome.blade.php
+++ Forum/resources/views/welcome.blade.php
@@ -1,15 +1,27 @@
-
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="pt-br">
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel Sidebar</title>
-    <link href="{{ asset('css/sidebar.css') }}" rel="stylesheet">
-    <script src="{{ asset('js/sidebar.js') }}" defer></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <meta charset="utf-8" />
 </head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="icon" href="{{ asset('images/masterIcon.ico') }}" type="image/x-icon">
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<link rel="stylesheet" href="{{ asset('vendor/font-awesome/css/all.min.css') }}">
+<link href="{{ asset('css/sidebar.css') }}" rel="stylesheet">
+<script src="{{ asset('js/sidebar.js') }}" defer></script>
+<script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+</script>
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+<title> MasterForum - Laravel </title>
+</head>
+
 <body>
+    
     <div id="app">
         <div id="sidebar" class="sidebar">
             <div class="sidebar-header">
@@ -28,7 +40,7 @@
         <div class="navbar">
             <i class="fa fa-bars" id="btn-navbar"></i>
             <div class="titleWrapper">
-                <h1 class="Title">Forum</h1>
+                <h1 class="Title">MasterForum</h1>
             </div>
             <a href='users/1'>
                 <i class="fas fa-user-circle"></i>
@@ -37,9 +49,36 @@
                 <i class="fas fa-sign-out-alt"></i>
             </a>
         </div>
-        <div class="content">
-            <h1>AEEEEEEEE</h1>
-        </div>
     </div>
+    
+    <main>
+        @yield('content')
+    </main>
+
+    <footer class="footer">
+        <div class="container">
+            <ul class="list-inline">
+                <li class="list-inline-item">
+                    <a href="https://www.linkedin.com/in/davi-ryan-konuma-lima-62b00221b/" target="_blank" rel="noopener noreferrer">
+                        <i class="fab fa-linkedin"></i>
+                    </a>
+                </li>
+                <li class="list-inline-item">
+                    <a href="https://github.com/DaviRKL" target="_blank" rel="noopener noreferrer">
+                        <i class="fab fa-github"></i>
+                    </a>
+                </li>
+                <li class="list-inline-item">
+                    <a href="mailto:davirkl07@gmail.com">
+                        <i class="fas fa-envelope"></i>
+                    </a>
+                </li>
+            </ul>
+            <p>&copy; 2024 Davi Ryan Konuma Lima</p>
+        </div>
+    </footer>
+
 </body>
+
 </html>
+

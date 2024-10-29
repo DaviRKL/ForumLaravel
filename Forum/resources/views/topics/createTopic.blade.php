54f859fa8d99291b1d9daab88799fe246737f518
DaviRKL <davirkl07@gmail.com>
2024-10-29 11:35:42 -0300

    Adicao do select de categorias

--- Forum/resources/views/topics/createTopic.blade.php
+++ Forum/resources/views/topics/createTopic.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="create-post-container">
-    
+
     <form action="{{route('createTopic')}}" method="POST" class="create-post-form">
         <h2 class="create-post-title">Crie seu Topico!</h2>
         @csrf
@@ -10,16 +10,31 @@
             <label for="title" class="form-label">Titulo do Topico:</label>
             <input type="text" id="title" name="title" class="form-input" value="{{ old('title') }}" required>
             @error("title") <span>{{$message}}</span> @enderror
+
             <label for="description" class="form-label">Descrição do Topico:</label>
             <input type="text" id="description" name="description" class="form-input" value="{{ old('description') }}" required>
             @error("description") <span>{{$message}}</span> @enderror
+
             <label for="status" class="form-label">Status do Topico:</label>
             <input type="number" id="status" name="status" class="form-input" value="{{ old('status') }}" required>
             @error("status") <span>{{$message}}</span> @enderror
+
             <label for="image" class="form-label">Imagem do Topico:</label>
             <input type="text" id="image" name="image" class="form-input" value="{{ old('image') }}" required>
             @error("image") <span>{{$message}}</span> @enderror
+
+            <label for="category" class="form-label">Categoria do Tópico:</label>
+            <select id="category" name="category_id" class="form-input" required>
+                <option value="">Selecione uma categoria</option>
+                @foreach($categories as $category)
+                    <option value="{{ $category->id }}" {{ old('category_id') == $category->id ? 'selected' : '' }}>
+                        {{ $category->name }}
+                    </option>
+                @endforeach
+            </select>
+            @error("category_id") <span>{{$message}}</span> @enderror
         </div>
+
         <input type="submit" class="submit-button" value="Enviar">
     </form>
 </div>
